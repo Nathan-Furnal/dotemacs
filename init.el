35dfebfb48b440ece9ac9445026d33a7784ff5db
Nathan Furnal <nathan_furnal@hotmail.com>
2020-12-07 15:39:43 +0100

    Revamp python setup

--- init.el
+++ init.el
@@ -302,8 +302,8 @@
 	
         company-dabbrev-ignore-case t
 
-        ;; Wait a bit before completion.
-        company-idle-delay 0.3
+        ;; Don't way before completion.
+        company-idle-delay 0
 
 	;; No company-mode in shell & eshell
 	company-global-modes '(not eshell-mode shell-mode))
@@ -1109,3 +1109,16 @@
   (add-to-list 'global-mode-string '("" mode-line-keycast)))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(keycast gif-screencast yasnippet-snippets emmet-mode skewer-mode impatient-mode web-mode json-mode js2-refactor tide prettier-js rjsx-mode lsp-java jupyter ess hide-mode-line elpy julia-repl julia-mode cider clojure-mode sly elisp-lint package-lint buttercup dap-mode lsp-treemacs lsp-ui lsp-mode treemacs iedit multiple-cursors magit pandoc-mode markdown-mode deft org-noter shackle org-ref cdlatex auctex flycheck transpose-frame company which-key ctrlf flimenu imenu-list selectrum-prescient selectrum centaur-tabs doom-modeline modus-vivendi-theme modus-operandi-theme popup-kill-ring diminish use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
