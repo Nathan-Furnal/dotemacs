8e4348ac0206729ecb099e999dd81df240cd89ac
Nathan Furnal <nathan_furnal@hotmail.com>
2022-05-19 21:00:23 +0200

    minor changes

--- init.el
+++ init.el
@@ -76,6 +76,7 @@
   (global-hl-line-mode 1)			; Highlight the current line to make it more visible
   (setq create-lockfiles nil)                   ; lock files kill `npm start'
   (setq-default fill-column 80)		        ; Set fill column to 80 rather than 70, in all cases.
+  (pixel-scroll-precision-mode 1)	        ; Precision scrolling
 
   (setq backup-directory-alist
 	`(("." . ,(concat user-emacs-directory "backups"))))
@@ -182,7 +183,7 @@
 	modus-themes-paren-match '(intense)
 	modus-themes-mode-line '(moody borderless))
   (setq modus-themes-headings
-        (quote ((1 . (background overline variable-pitch 1.4))
+        (quote ((1 . (overline variable-pitch 1.4))
                 (2 . (overline rainbow variable-pitch 1.25))
                 (3 . (overline 1.1))
                 (t . (monochrome))))))
@@ -528,6 +529,8 @@
 (use-package org-modern
   :ensure t
   :defer t
+  :custom
+  (org-modern-table nil)
   :hook ((org-mode-hook . org-modern-mode)
 	 (org-agenda-finalize-hook . org-modern-agenda)))
 
@@ -678,8 +681,8 @@
   (lsp-completion-enable t)
   :hook ((css-mode-hook . lsp-deferred)
 	 (web-mode-hook . lsp-deferred)
-	 (js2-mode-hook . lsp-deferred)
 	 (c++-mode-hook . lsp-deferred)
+	 (js2-mode-hook . lsp-deferred)
 	 (c-mode-hook . lsp-deferred)
 	 (csharp-mode-hook . lsp-deferred)
 	 (sql-mode-hook . lsp-deferred)
@@ -879,16 +882,14 @@
   (setq python-indent-guess-indent-offset-verbose nil))
 
 ;; Hide the modeline for inferior python processes
-(use-package inferior-python-mode
-  :ensure nil
+
+(use-package hide-mode-line
+  :ensure t
+  :defer t
   :hook ((inferior-python-mode-hook . hide-mode-line-mode)
 	 (inferior-ess-r-mode-hook . hide-mode-line-mode)
 	 (pdf-view-mode-hook . hide-mode-line-mode)
 	 (shell-mode-hook . hide-mode-line-mode)))
-
-(use-package hide-mode-line
-  :ensure t
-  :defer t)
 
 (use-package poetry
   :ensure t
@@ -1024,6 +1025,11 @@
 (use-package ob-makefile
   :defer t)
 
+(use-package ob-php
+  :ensure t
+  :defer t
+  :commands (org-babel-execute:php))
+
 ;;;========================================
 ;;; Web development
 ;;;========================================
@@ -1037,11 +1043,6 @@
   :defer t
   :mode ("\\.js\\'" . rjsx-mode)
   :hook (rjsx-mode-hook . prettier-js-mode))
-
-(use-package prettier-js
-  :ensure t
-  :defer t
-  :after (rjsx-mode))
 
 ;; Importantly, I have to setup a jsconfig.json in the root folder of the project, see https://github.com/ananthakumaran/tide#javascript
 ;; Here is a template :
@@ -1457,7 +1458,7 @@
  '(custom-safe-themes
    '("0998a5646f4a322ba70ca51cf7db727cb75eec2cf1fca0a28442e72142b170ce" "74a50f18c8c88eac44dc73d7a4c0bbe1f3e72ff5971aac38fcf354ddad0d4733" "aa72e5b41780bfff2ff55d0cc6fcd4b42153386088a4025fed606c1099c2d9b8" "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "9f1d0627e756e58e0263fe3f00b16d8f7b2aca0882faacdc20ddd56a95acb7c2" "7397cc72938446348521d8061d3f2e288165f65a2dbb6366bb666224de2629bb" "bd3b9675010d472170c5d540dded5c3d37d83b7c5414462737b60f44351fb3ed" default))
  '(package-selected-packages
-   '(org-modern qt-pro-mode reason-mode merlin tuareg ocamlformat xr janet-mode haxe-mode modus-themes lsp-metals scala-mode csharp-mode meson-mode blacken php-mode zig-mode vertico marginalia docstr w3m masm-mode tree-sitter-langs tree-sitter lua-mode julia-snail julia-mode org python flymake-nasm nasm-mode gnuplot plantuml-mode yaml-mode maxima ox-hugo gif-screencast yasnippet-snippets cargo rustic rust-mode emmet-mode nodejs-repl impatient-mode web-mode json-mode js2-refactor tide prettier-js rjsx-mode ess numpydoc lsp-pyright poetry hide-mode-line racket-mode geiser-mit geiser flycheck-clj-kondo cider rainbow-delimiters paredit sly vterm elisp-lint package-lint buttercup dap-mode lsp-mode iedit magit pandoc-mode markdown-mode pdf-tools olivetti org-tree-slide ox-reveal imenu-list org-roam shackle org-ref cdlatex auctex flycheck transpose-frame treemacs company which-key orderless circadian moody exec-path-from-shell gcmh delight diminish use-package)))
+   '(ob-php org-modern qt-pro-mode reason-mode merlin tuareg ocamlformat xr janet-mode haxe-mode modus-themes lsp-metals scala-mode csharp-mode meson-mode blacken php-mode zig-mode vertico marginalia docstr w3m masm-mode tree-sitter-langs tree-sitter lua-mode julia-snail julia-mode org python flymake-nasm nasm-mode gnuplot plantuml-mode yaml-mode maxima ox-hugo gif-screencast yasnippet-snippets cargo rustic rust-mode emmet-mode nodejs-repl impatient-mode web-mode json-mode js2-refactor tide prettier-js rjsx-mode ess numpydoc lsp-pyright poetry hide-mode-line racket-mode geiser-mit geiser flycheck-clj-kondo cider rainbow-delimiters paredit sly vterm elisp-lint package-lint buttercup dap-mode lsp-mode iedit magit pandoc-mode markdown-mode pdf-tools olivetti org-tree-slide ox-reveal imenu-list org-roam shackle org-ref cdlatex auctex flycheck transpose-frame treemacs company which-key orderless circadian moody exec-path-from-shell gcmh delight diminish use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
