eec455867c25e62d02efe373cecdd8e718a28fc2
Nathan Furnal <nathanfurnal.dev@gmail.com>
2025-11-23 22:32:49 +0100

    custom vars update

--- custom.el
+++ custom.el
@@ -6,17 +6,16 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(ada-ts-mode ajrepl auctex bqn-mode buttercup cape cdlatex citar clj-refactor
-                 clojure-ts-mode compile-angel consult-denote corfu csv-mode
-                 dape difftastic diminish docker dockerfile-mode eglot elfeed
-                 elisp-lint embark-consult exunit fish-mode flycheck-clj-kondo
-                 flycheck-credo flymake-ruff forth-mode fuel geiser-chez
-                 geiser-chicken geiser-guile gnuplot go-mode gpr-ts-mode
-                 haskell-ts-mode hide-mode-line hindent inf-elixir janet-ts-mode
-                 jinx julia-snail julia-ts-mode kkp langtool lua-mode marginalia
-                 markdown-mode maxima mix modus-themes nov numpydoc orderless
-                 pandoc-mode parinfer-rust-mode pdf-tools pet puni
-                 rainbow-delimiters roc-ts-mode slime sweeprolog treesit-auto
-                 uniline vertico vterm zig-mode)))
+                 clojure-ts-mode compile-angel consult-denote corfu csv-mode dape
+                 difftastic diminish docker dockerfile-mode elfeed elisp-lint
+                 embark-consult exunit fish-mode flycheck-clj-kondo flycheck-credo
+                 flymake-ruff forth-mode fuel geiser-chez geiser-chicken geiser-guile
+                 gnuplot go-mode gpr-ts-mode haskell-ts-mode hide-mode-line hindent
+                 inf-elixir janet-ts-mode jinx julia-snail julia-ts-mode kkp langtool
+                 lua-mode marginalia markdown-mode maxima mix modus-themes nov numpydoc
+                 orderless pandoc-mode parinfer-rust-mode pdf-tools pet puni
+                 rainbow-delimiters roc-ts-mode slime sweeprolog treesit-auto uniline
+                 vertico vterm zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
