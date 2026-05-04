7deb5b50f26c47d0eaa83b39427463057b1c774a
Nathan Furnal <nathanfurnal.dev@gmail.com>
2026-05-04 21:25:28 +0200

    update minimal-emacs config

--- custom.el
+++ custom.el
@@ -5,18 +5,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ada-ts-mode agent-shell ajrepl auctex bqn-mode buttercup cape cdlatex citar cl-generic
-                 clj-refactor clojure-ts-mode compile-angel consult-denote corfu csv-mode
-                 dape difftastic diminish docker dockerfile-mode editorconfig ef-themes
-                 eglot elfeed elisp-lint embark-consult erc exec-path-from-shell exunit
-                 faceup fish-mode flycheck-credo flymake-ruff forth-mode fuel geiser-chez
-                 geiser-chicken geiser-guile gnuplot go-mode gpr-ts-mode haskell-ts-mode
-                 hide-mode-line hindent idlwave inf-elixir janet-ts-mode jinx julia-snail
-                 julia-ts-mode kkp langtool lua-mode marginalia markdown-mode maxima mix
-                 nov ntlm numpydoc orderless pandoc-mode parinfer-rust-mode pdf-tools peg
-                 pet puni python rainbow-delimiters roc-ts-mode slime so-long soap-client
-                 svg sweeprolog tramp treesit-auto uniline use-package verilog-mode
-                 vertico vterm which-key window-tool-bar zig-mode)))
+   '(ada-ts-mode agent-shell ajrepl auctex bind-key bqn-mode buttercup cape cdlatex citar
+                 cl-generic cl-lib clj-refactor clojure-ts-mode compile-angel
+                 consult-denote corfu csv-mode dape difftastic diminish docker
+                 dockerfile-mode editorconfig eglot eldoc elfeed elisp-lint
+                 embark-consult erc exec-path-from-shell external-completion exunit
+                 faceup fish-mode flycheck-credo flymake flymake-ruff forth-mode fuel
+                 geiser-chez geiser-chicken geiser-guile gnuplot go-mode gpr-ts-mode
+                 haskell-ts-mode hide-mode-line hindent idlwave inf-elixir janet-ts-mode
+                 jinx jsonrpc julia-snail julia-ts-mode kkp langtool let-alist lua-mode
+                 map marginalia markdown-mode maxima mix modus-themes nadvice nov ntlm
+                 numpydoc orderless org pandoc-mode parinfer-rust-mode pdf-tools peg pet
+                 project puni python rainbow-delimiters roc-ts-mode seq slime so-long
+                 soap-client svg sweeprolog track-changes tramp treesit-auto uniline
+                 use-package verilog-mode vertico vterm which-key window-tool-bar xref
+                 zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
